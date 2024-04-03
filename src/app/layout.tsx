7cec4c3b7883f2572dfefcd2558bc69686172c25
kevan (kevin anantha) <kevin.anantha@gmail.com>
2024-04-03 20:47:35 +0800

    nav

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import {GeistSans} from 'geist/font/sans'
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "UI",
+  description: "UI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={GeistSans.className}>{children}</body>
+    </html>
+  );
+}
